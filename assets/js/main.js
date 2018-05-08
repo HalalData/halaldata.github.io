825ea29464726426fe1567a3465192647454c21c
Asep Bagja Priandana <bepitulaz@gmail.com>
2018-05-08 17:11:40 +0800

    Querying Overpass API to search halal diet.

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,12 +10,12 @@ function stateChanged() {
 		if (ajaxRequest.status==200) {
 			plotlist=eval("(" + ajaxRequest.responseText + ")");
 			removeMarkers();
-			for (i=0;i<plotlist.length;i++) {
-				let plotll = new L.LatLng(plotlist[i].lat,plotlist[i].lon, true);
+			for (i=0;i<plotlist.elements.length;i++) {
+				let plotll = new L.LatLng(plotlist.elements[i].lat,plotlist.elements[i].lon, true);
 				let plotmark = new L.Marker(plotll);
-				plotmark.data=plotlist[i];
+				plotmark.data=plotlist.elements[i];
 				mainMap.addLayer(plotmark);
-				plotmark.bindPopup("<h3>"+plotlist[i].name+"</h3>"+plotlist[i].details);
+				plotmark.bindPopup("<h3>"+plotlist.elements[i].tags.name+"</h3>"+plotlist.elements[i].tags.amenity);
 				plotlayers.push(plotmark);
 			}
 		}
@@ -34,7 +34,7 @@ function askForPlots() {
 	let bounds = mainMap.getBounds();
 	let minll = bounds.getSouthWest();
 	let maxll = bounds.getNorthEast();
-	let msg = 'leaflet/findbybbox.cgi?format=leaflet&bbox='+minll.lng+','+minll.lat+','+maxll.lng+','+maxll.lat;
+	let msg = `https://lz4.overpass-api.de/api/interpreter?data=[out:json];node%20[diet=halal](${minll.lat},${minll.lng},${maxll.lat},${maxll.lng});out;`
 	ajaxRequest.onreadystatechange = stateChanged;
 	ajaxRequest.open('GET', msg, true);
 	ajaxRequest.send(null);
@@ -63,12 +63,16 @@ function main() {
     // create the tile layer with correct attribution
 	let osmUrl='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
 	let osmAttrib='Map data © <a href="https://openstreetmap.org">OpenStreetMap</a> contributors';
-	let osm = new L.TileLayer(osmUrl, {minZoom: 8, maxZoom: 12, attribution: osmAttrib});		
+	let osm = new L.TileLayer(osmUrl, {attribution: osmAttrib});		
 
 	// start the map in South-East England
-	mainMap.setView(new L.LatLng(51.3, 0.7),9);
+	mainMap.setView(new L.LatLng(51.3, 0.7),6);
     mainMap.addLayer(osm);
-    
+	
+	// adding search layer
+	let searchLayer = L.layerGroup().addTo(mainMap);
+	mainMap.addControl( new L.Control.Search({layer: searchLayer}) );
+
     askForPlots();
 	mainMap.on('moveend', onMapMove);
 }
